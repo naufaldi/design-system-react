aac9b16841c4aeb56667f15b5bfb07a99e5d5d5b
Naufaldi Rafif <naufaldirafif@Faiks-MacBook-Air.local>
2020-09-18 21:15:15 +0700

    add stories

--- my-component-library/src/components/Modal.js
+++ my-component-library/src/components/Modal.js
@@ -1,12 +1,23 @@
 import React from "react";
 import styled from "styled-components";
-import { typeScale } from "../utils";
-import { PrimaryButton } from "./Button";
-import { Illustration, CloseIcon } from "../assets";
+import { animated, useSpring } from "react-spring";
+import { typeScale, primaryFont } from "../utils";
+import { Illustrations, CloseIcon } from "../assets";
+import { PrimaryButton, SecondaryButton } from "./Buttons";
+import { EmailInput, PasswordInput } from "./TextFields";
+
+const getAnimation = showModal => {
+  return {
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0)` : `translateY(-200%)`
+  };
+};
+
 const ModalWrapper = styled.div`
   width: 800px;
   height: 500px;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+  font-family: ${primaryFont};
   background-color: ${props => props.theme.formElementBackground};
   color: ${props => props.theme.textOnFormElementBackground};
   display: flex;
@@ -15,9 +26,14 @@ const ModalWrapper = styled.div`
   align-items: center;
   position: relative;
   border-radius: 2px;
-  background: ${props => props.theme.formElementBackground};
 `;
-const SignUpHeader = styled.h3`
+
+const ColumnModalWrapper = styled(ModalWrapper)`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+const ModalHeader = styled.h3`
   font-size: ${typeScale.header3};
 `;
 
@@ -26,6 +42,7 @@ const SignUpText = styled.p`
   max-width: 70%;
   text-align: center;
 `;
+
 const CloseModalButton = styled.button`
   cursor: pointer;
   background: none;
@@ -38,20 +55,48 @@ const CloseModalButton = styled.button`
   padding: 0;
 `;
 
-export const SignUpModal = () => {
+export const SignUpModal = ({ showModal, setShowModal }) => {
   return (
-    <ModalWrapper>
-      <img src={Illustration.SignUp} alt="Sign up for an account!" />
-      <SignUpHeader>Sign Up</SignUpHeader>
-      <SignUpText>
-        Sign up today to get access to all of our content and features!
-      </SignUpText>
-      <PrimaryButton onClick={() => console.log("You signed up!")}>
-        Sign Up
-      </PrimaryButton>
-      <CloseModalButton aria-label="Close modal" onClick={() => console.log("You closed the modal!")}>
-      <CloseIcon />
-    </CloseModalButton>
-    </ModalWrapper>
+    <animated.div style={useSpring(getAnimation(showModal))}>
+      <ModalWrapper>
+        <img src={Illustrations.SignUp} alt="Sign up for an account!" />
+        <ModalHeader>Sign Up</ModalHeader>
+        <SignUpText>
+          Sign up today to get access to all of our content and features!
+        </SignUpText>
+        <PrimaryButton onClick={() => console.log("You signed up!")}>
+          Sign Up
+        </PrimaryButton>
+        <CloseModalButton
+          aria-label="Close modal"
+          onClick={() => setShowModal(false)}
+        >
+          <CloseIcon />
+        </CloseModalButton>
+      </ModalWrapper>
+    </animated.div>
   );
 };
+
+export const SignInModal = ({ showModal, setShowModal }) => (
+  <animated.div style={useSpring(getAnimation(showModal))}>
+    <ColumnModalWrapper>
+      <div>
+        <ModalHeader>Sign In</ModalHeader>
+        <EmailInput label="Email" placeholder="emmabostian@gmail.com" />
+        <PasswordInput label="Password" />
+        <SecondaryButton style={{ margin: "16px 16px 0 0" }}>
+          Sign Up
+        </SecondaryButton>
+        <PrimaryButton>Sign In</PrimaryButton>
+      </div>
+      <img src={Illustrations.SignIn} alt="Sign in to your account" />
+      <CloseModalButton
+        aria-label="Close modal"
+        onClick={() => setShowModal(false)}
+      >
+        <CloseIcon />
+      </CloseModalButton>
+    </ColumnModalWrapper>
+  </animated.div>
+);
